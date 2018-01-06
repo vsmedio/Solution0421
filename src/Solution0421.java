decff7a7d607f92bd460ed2254985ca999c60543
Arvis Abromovics <info@vsmedio.lv>
2018-01-06 02:28:07 +0100

    Divi vienadi vai atskirigi vardi

---
+++ src/Solution0421.java
@@ -0,0 +1,23 @@
+/* Сравнить имена
+Ввести с клавиатуры два имени, и если имена одинаковые вывести сообщение «Имена идентичны».
+Если имена разные, но их длины равны – вывести сообщение – «Длины имен равны».
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution0421
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String a = reader.readLine();
+        String b = reader.readLine();
+
+        if(a.equals(b)){
+            System.out.println("Имена идентичны");
+        } else if(a.length() == b.length()){
+            System.out.println("Длины имен равны");
+        }
+    }
+}
